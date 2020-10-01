e7ced24ba2e2ebc6ec103f7a20c2fed4c72c41e0
udayshree123 <udayshree123@gmail.com>
2020-10-01 19:02:52 +0530

    1

---
+++ src/main/java/simple5/App.java
@@ -0,0 +1,9 @@
+package simple5;
+
+public class App 
+{
+  public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+  }
+}
